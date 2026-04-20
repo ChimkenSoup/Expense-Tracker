bfe2f47a93e4f235ae835bd7c3b729c6810a75fa
Neil Tauro <neiltauro15@gmail.com>
2026-04-20 19:59:12 +0530

    Final project ready for Render

--- app/database.py
+++ app/database.py
@@ -1,23 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from asyncpg import Connection
-from uuid import uuid4
-from . import config
 from app.config import settings
 
 SQLALCHEMY_DATABASE_URL = f"{settings.database_url}"
 
-class UniqueNameConnection(Connection):
-    def _get_unique_id(self, prefix: str) -> str:
-        return f"__asyncpg_{prefix}_{uuid4().hex}__"
-
-
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={
-        "connection_class": UniqueNameConnection,
-        "statement_cache_size": 0,
-    }
 )
 
 AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, autocommit=False, autoflush=False)
--- app/models.py
+++ app/models.py
@@ -6,14 +6,15 @@ class Expense(Base):
     __tablename__ = "expenses"
     id = Column(Integer, primary_key=True, index = True)
     expense = Column(Integer, nullable=False)
-    description = Column(String)
-    mode = Column(String)
-    created_at = Column(TIMESTAMP(timezone = True), nullable = False , server_default=text('now()'))
+    description = Column(String(255))
+    mode = Column(String(50))
+    created_at = Column(TIMESTAMP, nullable = False , server_default=text('CURRENT_TIMESTAMP'))
     owner_id = Column(Integer,ForeignKey("users.id",ondelete="CASCADE"), nullable=False)
     
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index = True)
-    email = Column(String, nullable = False, unique = True)
-    password = Column(String, nullable = False)
-    created_at = Column(TIMESTAMP(timezone = True), nullable = False , server_default=text('now()'))
+    email = Column(String(255), nullable = False, unique = True)
+    password = Column(String(255), nullable = False)
+    created_at = Column(TIMESTAMP, nullable = False , server_default=text('CURRENT_TIMESTAMP'))
+
